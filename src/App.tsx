7201d2edda2743a31eb5903b74efa0c5a6dc8650
njal3001 <73302347+njal3001@users.noreply.github.com>
2021-05-21 18:17:58 +0200

    Created header with vanilla CSS (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import { FunctionComponent } from "react";
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./header";
 
-import { Navbar, NavItem, NavDropdown, Nav, Form, Button, FormControl } from 'react-bootstrap';
+const App = () => {
+  return <Header />;
+};
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-       <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
-      </div>
-    );
-  }
-}
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-//import Navbar from "react-bootstrap/Navbar";
-const Home = () => {
-  return <h1>Home Page</h1>;
-}
-*/
 export default App;
