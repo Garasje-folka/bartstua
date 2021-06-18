cd069ff29054f853a262415768e98c493884997f
njal3001 <73302347+njal3001@users.noreply.github.com>
2021-06-18 15:58:11 +0200

    Fixed theme import bug

--- src/components/bookingEvent/bookingEvent.styled.ts
+++ src/components/bookingEvent/bookingEvent.styled.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import { theme } from "../../theme";
+import { themeConfig } from "../../app.theme";
 
 export const SessionContainer = styled.div`
   min-width: 150px;
   min-height: 150px;
   width: 10vw;
   height: 10vw;
-  margin-bottom: ${theme.alignment.margin.REGULAR};
-  margin-top: ${theme.alignment.margin.REGULAR};
-  box-shadow: ${theme.shadow.REGULAR};
+  margin-bottom: ${themeConfig.alignment.margin.REGULAR};
+  margin-top: ${themeConfig.alignment.margin.REGULAR};
+  box-shadow: ${themeConfig.shadow.REGULAR};
 `;
