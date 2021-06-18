a55abd7537b900fffd7e1e923168a157510779bc
njal3001 <73302347+njal3001@users.noreply.github.com>
2021-06-18 16:59:39 +0200

    Fixed bookingDate increment bug (#32)

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { BookingDay } from "../components/bookingDay/bookingDay";
 import { Container, Row, Col } from "react-bootstrap";
 import { DateDay } from "../services/bookingManagement/interfaces";
 import createDateDayFromDate from "../services/bookingManagement/helpers/createDateDay";
+import { Button } from "../components/button";
+import addToDateDay from "../services/bookingManagement/helpers/addToDateDay";
 
 const Booking: React.FC = () => {
-  const getBookingDates = (startDay: DateDay, columns: number) => {
+  const [startDay, setStartDay] = useState<DateDay>(
+    createDateDayFromDate(new Date())
+  );
+
+  const incrementStartDate = (positive: boolean) => {
+    const incrementDay = positive ? 1 : -1;
+
+    setStartDay((prevStartDay) => addToDateDay(prevStartDay, incrementDay));
+  };
+
+  const getBookingDates = (columns: number) => {
     const bookingDates = [];
     for (let i = 0; i < columns; i++) {
-      const offsetDay = {
-        ...startDay,
-        day: startDay.day + i,
-      };
+      const offsetDay = addToDateDay(startDay, i);
 
       bookingDates.push(
         <Col key={i}>
@@ -23,9 +32,13 @@ const Booking: React.FC = () => {
     return bookingDates;
   };
   return (
-    <Container>
-      <Row>{getBookingDates(createDateDayFromDate(new Date()), 5)}</Row>
-    </Container>
+    <>
+      <Button onClick={() => incrementStartDate(false)}> Bakover </Button>
+      <Button onClick={() => incrementStartDate(true)}> Forover </Button>
+      <Container>
+        <Row>{getBookingDates(5)}</Row>
+      </Container>
+    </>
   );
 };
 
