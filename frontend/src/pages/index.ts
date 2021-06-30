38a71605f0346398c446582f058e0c42035eaebe
Minh Kenny Cao Nguyen <61881445+MKenny5013@users.noreply.github.com>
2021-06-30 16:53:44 +0200

    design of register page

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,6 +2,6 @@ export * from "./about";
 export * from "./booking";
 export * from "./home";
 export * from "./signIn/signIn";
-export * from "./register";
+export * from "./signUp/register";
 export * from "./verify";
 export * from "./passwordChange";
