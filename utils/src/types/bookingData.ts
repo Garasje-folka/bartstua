f5761ee46e41c3fd6850b7f62bca3f33139aa545
haryp2309 <hary2309@gmail.com>
2021-07-03 16:29:56 +0200

    Update types for BookingData, DateDay and DateHour

--- utils/src/types/bookingData.ts
+++ utils/src/types/bookingData.ts
@@ -4,13 +4,9 @@ import { MAX_EVENT_SPACES } from "./constants";
 
 // TODO: Don't know if this is the correct way to do nested schemas
 export const bookingDataSchema = yup.object({
-  date: dateHourSchema.required(),
+  date: dateHourSchema,
   spaces: yup.number().min(1).max(MAX_EVENT_SPACES).required(),
   uid: yup.string().required(),
 });
 
-export type BookingData = {
-  date: DateHour;
-  spaces: number;
-  uid: string;
-};
+export type BookingData = yup.InferType<typeof bookingDataSchema>;
--- utils/src/types/dateDay.ts
+++ utils/src/types/dateDay.ts
@@ -2,8 +2,8 @@ import * as yup from "yup";
 
 export const dateDaySchema = yup.object({
   year: yup.number().required(),
-  month: yup.number().required(),
-  day: yup.number().required(),
+  month: yup.number().min(1).max(12).required(),
+  day: yup.number().min(1).max(31).required(),
 });
 
 export type DateDay = yup.InferType<typeof dateDaySchema>;
