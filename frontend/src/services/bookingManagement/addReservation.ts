bab704b4db44539515db50cd0dab828dc49559d7
haryp2309 <hary2309@gmail.com>
2021-07-03 16:51:28 +0200

    Removed unused imports

--- frontend/src/services/bookingManagement/addReservation.ts
+++ frontend/src/services/bookingManagement/addReservation.ts
@@ -1,7 +1,5 @@
 import firebase from "firebase";
 import { BookingData } from "utils";
-import { auth } from "../fireConfig";
-import { signInAnonymously } from "../userManagement/signInAnonymously";
 
 // TODO: Add proper error handling
 
