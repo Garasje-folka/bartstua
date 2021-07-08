13c7d93ad2fa5253e79a35b6594687e7c02d271f
njal3001 <njal_telsto@hotmail.com>
2021-07-08 15:28:59 +0200

    Some frontend cleanup (#97)

--- frontend/src/services/bookingManagement/addReservation.ts
+++ frontend/src/services/bookingManagement/addReservation.ts
@@ -1,15 +1,13 @@
 import firebase from "firebase";
 import { BookingRequest } from "utils";
 
-// TODO: Add proper error handling
-
 // Returns client secret which is used for payment
 const addReservation = async (booking: BookingRequest) => {
   const call = firebase.functions().httpsCallable("addReservation");
   try {
     await call(booking);
   } catch (error) {
-    console.log(error);
+    // TODO: Add proper error handling
     throw error;
   }
 };
