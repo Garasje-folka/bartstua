43b29e75919bedaf9c96ac57f4584827a6bb05af
Minh Kenny Cao Nguyen <61881445+MKenny5013@users.noreply.github.com>
2021-07-09 13:30:47 +0200

    deleted the background color in register

--- frontend/src/pages/signUp/signUp.styled.ts
+++ frontend/src/pages/signUp/signUp.styled.ts
@@ -4,7 +4,6 @@ import { FormContainer, InputField, SubmitButton } from "../../components/form";
 import saunaImg from "../../media/sauna.jpeg";
 
 export const Background = styled.div`
-  background-color: #5ab9ea;
   width: 100%;
   height: 100%;
   min-height: 100vh;
