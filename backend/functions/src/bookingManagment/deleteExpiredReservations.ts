463a39614f6d0206ed928c5bec8e4b41034dd96c
njal3001 <njal_telsto@hotmail.com>
2021-07-14 22:56:49 +0200

    Created scheduled function for deleting expired reservations (#121)

--- backend/functions/src/bookingManagment/deleteExpiredReservations.ts
+++ backend/functions/src/bookingManagment/deleteExpiredReservations.ts
@@ -1,6 +1,20 @@
 import * as functions from "firebase-functions";
-import { RESERVATION_EXPIRATION_TIME } from "utils/dist/bookingManagement/constants";
+import * as admin from "firebase-admin";
+import { RESERVATION_CLEARING_INTERVAL } from "./constants";
+import { RESERVATIONS } from "utils/dist/bookingManagement/constants";
+import { isExpiredReservation } from "utils/dist/bookingManagement/helpers";
+import { deleteReservation } from "./helpers";
+import { ReservationData } from "utils/dist/bookingManagement/types";
 
 export const deleteExpiredReservations = functions.pubsub
-  .schedule(`every ${RESERVATION_EXPIRATION_TIME} minutes`)
-  .onRun((context) => {});
+  .schedule(`every ${RESERVATION_CLEARING_INTERVAL} minutes`)
+  .onRun(async (context) => {
+    // TODO: Get expired reservations with queries instead
+    const reservations = await admin.firestore().collection(RESERVATIONS).get();
+    for (const reservation of reservations.docs) {
+      const { uid, ...reservationData } = reservation.data();
+      if (isExpiredReservation(reservationData as ReservationData)) {
+        await deleteReservation(reservation.id);
+      }
+    }
+  });
--- backend/functions/src/config.ts
+++ backend/functions/src/config.ts
@@ -1,3 +1,16 @@
 import * as admin from "firebase-admin";
+import { PubSub } from "@google-cloud/pubsub";
+import { RESERVATION_CLEARING_INTERVAL } from "./bookingManagment/constants";
 
 admin.initializeApp();
+
+// Used for emulator, remove in production
+const pubsub = new PubSub({
+  apiEndpoint: "localhost:8085",
+});
+
+setInterval(() => {
+  const SCHEDULED_FUNCTION_TOPIC =
+    "firebase-schedule-deleteExpiredReservations";
+  pubsub.topic(SCHEDULED_FUNCTION_TOPIC).publishJSON({});
+}, RESERVATION_CLEARING_INTERVAL * 60 * 1000);
