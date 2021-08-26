d5c4fc52a1931c1af04265c26b01cb7576298b33
njal3001 <njal_telsto@hotmail.com>
2021-08-26 16:48:50 +0200

    Small cleanup (#153)

--- backend/functions/src/config.ts
+++ backend/functions/src/config.ts
@@ -14,71 +14,79 @@ import {
 admin.initializeApp();
 
 // Fill next two months with events
+// TODO: Fix 'Exception occurred in retry method that was not classified as transient' error
 // TODO: Hardcoding location and event duration for know,
 //       fix later
-const startDate = createDateDayFromDate(new Date());
-let batch = admin.firestore().batch();
-let writes = 0;
-for (let i = 0; i <= 60; i++) {
-  const d = addToDateDay(startDate, i);
+const initializeEvents = async () => {
+  const batchSize = 500;
+  const startDate = createDateDayFromDate(new Date());
+  let batch = admin.firestore().batch();
+  let writes = 0;
+  for (let i = 0; i <= 60; i++) {
+    const d = addToDateDay(startDate, i);
 
-  // Drop-In events
-  for (let h = BOOKING_STARTING_TIME; h < BOOKING_ENDING_TIME; h++) {
-    const time = {
-      ...d,
-      hour: h,
-      minute: 0,
-    };
-    const docId = getEventId(time);
-    const ref = admin
-      .firestore()
-      .collection("locations")
-      .doc(EventLocation.loation1)
-      .collection(getEventCollectionName(BookingType.dropIn))
-      .doc(docId);
+    // Drop-In events
+    for (let h = BOOKING_STARTING_TIME; h < BOOKING_ENDING_TIME; h++) {
+      const time = {
+        ...d,
+        hour: h,
+        minute: 0,
+      };
+      const docId = getEventId(time);
+      const ref = admin
+        .firestore()
+        .collection("locations")
+        .doc(EventLocation.loation1)
+        .collection(getEventCollectionName(BookingType.dropIn))
+        .doc(docId);
 
-    batch.create(ref, {
-      spacesTaken: 0,
-      time: time,
-    });
+      batch.create(ref, {
+        spacesTaken: 0,
+        time: time,
+      });
 
-    writes++;
-    if (writes === 500) {
-      batch.commit();
-      batch = admin.firestore().batch();
-      writes = 0;
+      writes++;
+      if (writes === batchSize) {
+        await batch.commit();
+        batch = admin.firestore().batch();
+        writes = 0;
+      }
+    }
+
+    // Booking events
+    for (let h = BOOKING_STARTING_TIME; h < BOOKING_ENDING_TIME; h += 2) {
+      const time = {
+        ...d,
+        hour: h,
+        minute: 0,
+      };
+      const docId = getEventId(time);
+      const ref = admin
+        .firestore()
+        .collection("locations")
+        .doc(EventLocation.loation1)
+        .collection(getEventCollectionName(BookingType.booking))
+        .doc(docId);
+
+      batch.create(ref, {
+        taken: false,
+        time: time,
+      });
+
+      writes++;
+      if (writes === batchSize) {
+        await batch.commit();
+        batch = admin.firestore().batch();
+        writes = 0;
+      }
     }
   }
+  if (writes > 0) await batch.commit();
+};
 
-  // Booking events
-  for (let h = BOOKING_STARTING_TIME; h < BOOKING_ENDING_TIME; h += 2) {
-    const time = {
-      ...d,
-      hour: h,
-      minute: 0,
-    };
-    const docId = getEventId(time);
-    const ref = admin
-      .firestore()
-      .collection("locations")
-      .doc(EventLocation.loation1)
-      .collection(getEventCollectionName(BookingType.booking))
-      .doc(docId);
-
-    batch.create(ref, {
-      taken: false,
-      time: time,
-    });
-
-    writes++;
-    if (writes === 500) {
-      batch.commit();
-      batch = admin.firestore().batch();
-      writes = 0;
-    }
-  }
-}
-if (writes > 0) batch.commit();
+initializeEvents().catch((error) => {
+  console.log(error);
+});
 
 if (process.env.FUNCTIONS_EMULATOR) {
   const pubsub = new PubSub({
